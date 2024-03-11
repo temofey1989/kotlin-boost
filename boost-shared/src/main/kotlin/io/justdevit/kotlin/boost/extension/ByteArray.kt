3c29dffe001c0c1cc7a2f1f278438c58659003ca
Artyom Gornostayev <artyom.gornostayev@pbktechnology.cz>
2024-03-11 18:37:08 +0100

    [N/A] String and ByteArray extension.

---
+++ boost-shared/src/main/kotlin/io/justdevit/kotlin/boost/extension/ByteArray.kt
@@ -0,0 +1,31 @@
+package io.justdevit.kotlin.boost.extension
+
+import java.util.Base64
+
+/**
+ * Encodes a byte array into a Base64 string.
+ *
+ * @return the Base64 encoded string.
+ */
+fun ByteArray.encodeToString(): String = Base64.getEncoder().encodeToString(this)
+
+/**
+ * Encodes the given byte array using Base64 encoding.
+ *
+ * @return The encoded byte array.
+ */
+fun ByteArray.encode(): ByteArray = Base64.getEncoder().encode(this)
+
+/**
+ * Decodes the [ByteArray] into a [String] using Base64 decoding.
+ *
+ * @return The [String] representation of the decoded [ByteArray].
+ */
+fun ByteArray.decodeToString(): String = String(this.decode())
+
+/**
+ * Decodes the given [ByteArray] using Base64 decoding.
+ *
+ * @return The decoded [ByteArray].
+ */
+fun ByteArray.decode(): ByteArray = Base64.getDecoder().decode(this)
--- boost-shared/src/main/kotlin/io/justdevit/kotlin/boost/extension/String.kt
+++ boost-shared/src/main/kotlin/io/justdevit/kotlin/boost/extension/String.kt
@@ -1,5 +1,6 @@
 package io.justdevit.kotlin.boost.extension
 
+import java.text.Normalizer
 import java.time.Duration
 import java.time.LocalDate
 import java.time.LocalDateTime
@@ -10,6 +11,32 @@ import java.util.Locale
 import java.util.UUID
 import kotlin.contracts.ExperimentalContracts
 import kotlin.contracts.contract
+
+private val BASE64_REGEX = Regex("^([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==)?$")
+
+/**
+ * Checks if the current string is a valid Base64 encoded string.
+ *
+ * @return `true` if the string is a valid Base64 encoded string, `false` otherwise.
+ */
+fun String.isBase64() = BASE64_REGEX.matches(this)
+
+/**
+ * Encodes the current string into a Base64 encoded string.
+ *
+ * This method converts the current string into a byte array and then encodes it
+ * using Base64 encoding. The resulting Base64 encoded string is returned.
+ *
+ * @return The Base64 encoded string representation of the current string.
+ */
+fun String.encode() = this.toByteArray().encodeToString()
+
+/**
+ * Decodes the current [String] into a [ByteArray] using Base64 decoding.
+ *
+ * @return The [String] representation of the decoded [ByteArray].
+ */
+fun String.decode() = this.toByteArray().decodeToString()
 
 /**
  * Throws an [IllegalArgumentException] if the string is empty or contains only whitespace characters.
@@ -294,4 +321,46 @@ fun String?.hasText(): Boolean {
         returnsNotNull()
     }
     return !isNullOrBlank()
+}
+
+/**
+ * Removes diacritics (accented characters) from the given string.
+ *
+ * @receiver The string from which to remove diacritics.
+ * @return A new string without diacritics.
+ */
+fun String.removeDiacritics() =
+    Normalizer
+        .normalize(this, Normalizer.Form.NFD)
+        .replace("\\p{InCombiningDiacriticalMarks}+".toRegex(), "")
+
+/**
+ * Executes the specified action if this string is equal to the provided value.
+ *
+ * @param value The value to compare against.
+ * @param action The action to execute if this string is equal to the provided value.
+ */
+inline fun String?.ifEquals(value: String?, action: () -> Unit) {
+    if (this == value) action()
+}
+
+/**
+ * Executes the specified action if the value of this string is not equal to the specified value.
+ *
+ * @param value The value to compare this string against.
+ * @param action The action to be executed if the strings are not equal.
+ */
+inline fun String?.ifNotEquals(value: String?, action: () -> Unit) {
+    if (this != value) action()
+}
+
+/**
+ * Executes the `action` lambda on the [String] object only if it's not `null` or blank.
+ *
+ * @param action The action to execute on the [String] object if it's not `null` or blank.
+ */
+fun String?.ifNotBlank(action: String.() -> Unit) {
+    if (!isNullOrBlank()) {
+        this.action()
+    }
 }
