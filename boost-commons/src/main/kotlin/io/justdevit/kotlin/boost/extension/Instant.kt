542196336d9e376ad1b020920c7ac7e449cdf80a
Artyom Gornostayev <temofey1989@gmail.com>
2025-05-02 16:33:04 +0200

    feat: Helpful function for Instant

--- boost-commons/src/main/kotlin/io/justdevit/kotlin/boost/extension/Instant.kt
+++ boost-commons/src/main/kotlin/io/justdevit/kotlin/boost/extension/Instant.kt
@@ -2,16 +2,10 @@ package io.justdevit.kotlin.boost.extension
 
 import java.time.Instant
 import java.time.Instant.now
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.ZoneId.systemDefault
 import java.time.ZoneOffset.UTC
-
-/**
- * Checks if this [Instant] is between the given start and end [Instant]s (inclusive).
- *
- * @param from The start [Instant].
- * @param to The end [Instant].
- * @return `true` if this [Instant] is between the start and end [Instant]s (inclusive), `false` otherwise.
- */
-fun Instant.isBetween(from: Instant, to: Instant) = !isBefore(from) && !isAfter(to)
 
 /**
  * Represents the current year as an integer.
@@ -23,3 +17,26 @@ val currentYear: Int
         .atOffset(UTC)
         .toLocalDate()
         .year
+
+/**
+ * Checks if this [Instant] is between the given start and end [Instant]s (inclusive).
+ *
+ * @param from The start [Instant].
+ * @param to The end [Instant].
+ * @return `true` if this [Instant] is between the start and end [Instant]s (inclusive), `false` otherwise.
+ */
+fun Instant.isBetween(from: Instant, to: Instant) = !isBefore(from) && !isAfter(to)
+
+/**
+ * Converts the [Instant] to a [LocalDate] using the system default time zone.
+ *
+ * @return the [LocalDate] representation of this [Instant].
+ */
+fun Instant.toLocalDate(): LocalDate = atZone(systemDefault()).toLocalDate()
+
+/**
+ * Converts this [Instant] to a [LocalDateTime] in the system default time zone.
+ *
+ * @return The [LocalDateTime] representation of this [Instant] in the system default time zone.
+ */
+fun Instant.toLocalDateTime(): LocalDateTime = atZone(systemDefault()).toLocalDateTime()
